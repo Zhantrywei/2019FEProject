31fdc3fb0a1ac3fddb5f145dadd51e5c0658f283
Zhantrywei <844945330@qq.com>
2019-06-18 11:19:50 +0800

    note 5

--- study-notes/201905-精通JavaScript开发/5-设计模式-创建型/5-5-单例模式.js
+++ study-notes/201905-精通JavaScript开发/5-设计模式-创建型/5-5-单例模式.js
@@ -1,23 +1,23 @@
 // 单例模式
 var element = {
-    allElements: [],
-    get: function(id) {
-      var elem = document.getElementById(id);
-      this.allElements.push(elem);
-      return elem;
+        allElements: [],
+        get: function(id) {
+            var elem = document.getElementById(id);
+            this.allElements.push(elem);
+            return elem;
+        },
+        create: function(type) {
+            var elem = document.createElement(type);
+            this.allElements.push(elem);
+            return elem;
+        },
+        getAllElements: function() {
+            return this.allElements;
+        }
     },
-    create: function(type) {
-      var elem = document.createElement(type);
-      this.allElements.push(elem);
-      return elem;
-    },
-    getAllElements: function() {
-      return this.allElements;
-    }
-  },
-  header = element.get("header"),
-  input = element.create("input"),
-  allElements = element.getAllElements();
+    header = element.get("header"),
+    input = element.create("input"),
+    allElements = element.getAllElements();
 console.log(allElements.length); // 2
 
 // 使用自执行函数的单例模式
@@ -35,12 +35,65 @@ var cookie = (function() {
         cookies[unescape(cookie[0])] = unescape(cookie[1]);
     });
     return {
-        get: function(name){
+        get: function(name) {
             return cookies[name] || "";
         },
-        set: function(name, value){
+        set: function(name, value) {
             cookies[name] = value;
             document.cookie = escape(name) + "=" + escape(value);
         }
-    }
+    };
 })();
+
+// 设置命名空间
+var myProject = {
+    data: {
+        ajax: {
+            get: function(url, callback) {
+                var xhr = new XMLHttpRequest(),
+                    STATE_LOADED = 4,
+                    STATUS_OK = 200;
+                xhr.onreadystatechange = function() {
+                    if (xhr.readyState !== STATE_LOADED) {
+                        return;
+                    }
+                    if (xhr.status === STATUS_OK) {
+                        callback(xhr.responseText);
+                    }
+                };
+                xhr.open("GET", url);
+                xhr.send();
+            }
+        }
+    }
+};
+
+// 命名空间创建后,使用点号标记法可以增加命名空间
+myProject.data.cookies = {
+    get: function(name) {
+        var output = "",
+            escapedName = escpae(name),
+            start = document.cookie.indexOf(escapedName + "="),
+            end = document.cookie.indexOf(";", start);
+        end = end === -1 ? document.cookie.length - 1 : end;
+        if (start >= 0) {
+            output = document.cookie.substring(
+                start + escapedName.length + 1,
+                end
+            );
+        }
+        return unescape(output);
+    },
+
+    set: function(name, value) {
+        document.cookie = escape(name) + "=" + escape(value);
+    }
+};
+
+
+// 使用点标记法,通过命名空间调用执行方法
+myProject.data.ajax.get("/user/12345",function(res){
+  console.log("res: ",res);
+})
+myProject.data.cookies.set("company","AKQA")
+console.log(myProject.data.cookies.get("company"))
