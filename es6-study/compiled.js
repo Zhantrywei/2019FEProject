3f1d736b3c688732c887f9aa690f24c718ffa0de
Zhantrywei <844945330@qq.com>
2019-10-10 17:51:22 +0800

    es6

---
+++ es6-study/compiled.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var arr = [1, 2, 3];
+console.log(arr.map(function (item) {
+  return item + 1;
+}));
+{
+  var b = 123;
+}
---
+++ es6-study/example.js
@@ -0,0 +1,7 @@
+var arr = [1, 2, 3];
+console.log(arr.map(item => item + 1));
+
+
+{
+    let b = 123;
+}
